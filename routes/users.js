6bc45d180be01a028b1f505c8bfdc84fdc2bc011
AyoubXwire <benayoubid@gmail.com>
2019-02-05 10:29:27 +0100

    Make the 'validateFrom' function async and returns a promise

--- routes/users.js
+++ routes/users.js
@@ -16,46 +16,20 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
-router.post('/register', (req, res) => {
-    const { username, email, password, password2 } = req.body;
-    let errors = validateForm(username, email, password, password2);
-
-    // Handle the form
-    if(errors.length > 0) {
-        console.log(errors);
-        res.render('register');
-    } else {
-        // Create the user
-        const newUser = new User({
-            username: req.body.username,
-            email: req.body.email
-        });
-        User.register(newUser, req.body.password, (err, user) => {
-            if(err) {
-                console.log(err.message);
-                return res.redirect('/users/register');
-            }
-            passport.authenticate('local')(req, res, () => {
-                res.redirect('/shops/nearby');
-            });
-        });
-    }
-});
-
 router.post('/login', passport.authenticate('local', {
     successRedirect: '/shops/nearby',
     failureRedirect: '/'
 }), (req, res) => {
     User.findOne({username: req.body.username})
-        .then(user => {
-            if(user === undefined || user.password !== req.body.password) {
-                res.redirect('/')
-            }
-            else {
-                res.redirect('/shops/nearby');
-            }
-        })
-        .catch(err => console.log(err));
+    .then(user => {
+        if(user === undefined || user.password !== req.body.password) {
+            res.redirect('/')
+        }
+        else {
+            res.redirect('/shops/nearby');
+        }
+    })
+    .catch(err => console.log(err));
 });
 
 router.get('/logout', (req, res) => {
@@ -63,25 +37,56 @@ router.get('/logout', (req, res) => {
     res.redirect('/')
 });
 
+router.post('/register', (req, res) => {
+    const { username, email, password, password2 } = req.body;
+    validateForm(username, email, password, password2)
+        .then((validUser) => {
+            User.register(validUser, req.body.password, (err, user) => {
+                if(err) {
+                    console.log(err.message);
+                    return res.redirect('/users/register');
+                }
+                passport.authenticate('local')(req, res, () => {
+                    res.redirect('/shops/nearby');
+                });
+            });
+        })
+        .catch(errors => {
+            console.log(errors);
+            res.render('register');
+        });
+});
+
+// Make sure the form is valid and return an array of errors if any
 function validateForm(username, email, password, password2) {
-    let errors = [];
-
-    // Check required fields
-    if(!username || !email || !password || !password2) {
-        errors.push({ msg: 'Please fill in all the fields' });
-    }
-
-    // Check password strength
-    if(/^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/.test(password) == false){
-        errors.push({ msg: 'Make sure your password is at least 8 characters long and contains uppercase, lowercase, number and a symbol' });
-    }
-
-    // Check passwords match
-    if(password !== password2) {
-        errors.push({ msg: 'Passwords do not match' });
-    }
-
-    return errors;
+    return new Promise((resolve, reject) => {
+        let errors = [];
+    
+        // Check required fields
+        if(!username || !email || !password || !password2) {
+            errors.push('Please fill in all fields');
+        }
+    
+        // Check password strength
+        if(/^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/.test(password) == false){
+            errors.push('Password shouldt be at least 8 characters long and contains uppercase, lowercase, number and a symbol');
+        }
+    
+        // Check passwords match
+        if(password !== password2) {
+            errors.push('Passwords do not match');
+        }
+    
+        if(errors.length > 0) {
+            reject(errors);
+        } else {
+            const validUser = new User({
+                username: username,
+                email: email
+            });
+            resolve(validUser);
+        }
+    });
 }
 
 module.exports = router;
