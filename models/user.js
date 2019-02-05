6ca7a9d351d124c622672b58008822a0b755e6b6
AyoubXwire <benayoubid@gmail.com>
2019-02-05 07:25:51 +0100

    Add functions to like, dislike and remove a shop from preferred

--- models/user.js
+++ models/user.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 const passportLocalMongoose = require('passport-local-mongoose');
 const schema = mongoose.Schema;
 
+// 2 minutes for testing purposes
+const dislikeTimeout = 2 * 60 * 1000;
+
 const UserSchema = new schema({
     username: {
         type: String,
@@ -32,6 +35,36 @@ const UserSchema = new schema({
 });
 
 UserSchema.plugin(passportLocalMongoose, {usernameField: 'username'});
+
+// Add a shop to preferred shops
+UserSchema.methods.like = function(shop) {
+    if(this.liked.indexOf(shop) === -1) {
+        this.liked.push(shop);
+        return this.save();
+    }
+}
+
+// Remove a shope from the preferred shops
+UserSchema.methods.removeLiked = function(shop) {
+    if(this.liked.indexOf(shop) !== -1) {
+        this.liked.remove(shop);
+        return this.save();
+    }
+}
+
+// Dislike a shop, so it will get a timeout before getting removed from the disliked shops
+UserSchema.methods.dislike = function(shop) {
+    if(this.disliked.indexOf(shop) === -1) {
+        this.disliked.push(shop);
+
+        // Set the timeout
+        setTimeout(() => {
+            this.disliked.remove(shop);
+            this.save();
+        }, dislikeTimeout);
+        return this.save();
+    }
+}
 
 const User = mongoose.model('User', UserSchema);
 
